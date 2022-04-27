0d1e28e3b9f6e0a6de844364d7c6e437a8bee064
jungms4536 <eh4536@naver.com>
2022-04-27 16:36:49 +0900

    0429 update

--- sentimental_analysis/sentimentalAnalysis.py
+++ sentimental_analysis/sentimentalAnalysis.py
@@ -18,7 +18,7 @@ tweet_tokenizer = TweetTokenizer()
 
 class sentimental_analysis:
     def __init__(self,twitter_data):
-        self.twitter_data = twitter_data
+        self.twitter_data = twitter_data[:100]
         self.twitter_data['clean_text'] = np.NaN
         self.twitter_data['vader'] = np.NaN
         self.twitter_data['textblob'] = np.NaN
@@ -27,7 +27,7 @@ class sentimental_analysis:
         self.twitter_data = self.twitter_data.drop(self.twitter_data.columns[[0,1,2,3,4,5,6,7,10,11,12]],axis=1)
         korean = re.compile('[\u3131-\u3163\uac00-\ud7a3]+')
         clean_tweet = []
-        tweet = self.twitter_data.loc[:1800000]
+        tweet = self.twitter_data.loc[:100]
         if option == 'textblob':
             for index,row in tweet.iterrows():
                 tweet_list_corpus = re.sub(korean, '', row['text'])
@@ -56,11 +56,10 @@ class sentimental_analysis:
                 word_list = [stemmer.stem(word) for word in word_list]
                 text = ' '.join(word_list)
                 self.twitter_data.loc[index,'clean_text'] = text
-        self.twitter_data.to_csv('clean_ver_tweet.csv',mode='w')
         
     def sentimental_vader(self,file_name):            
         
-        result_tweet = pd.read_csv(file_name)
+        result_tweet = self.twitter_data
         #전처리된 csv파일 다시 reload
         text_list = list(result_tweet['clean_text'])
         senti_analyzer = SentimentIntensityAnalyzer()
@@ -75,7 +74,7 @@ class sentimental_analysis:
         #감정 분석 점수 데이터프레임에 추가
     
     def sentimental_flair(self,file_name):
-        result_tweet = pd.read_csv(file_name)
+        result_tweet = self.twitter_data
         text_list = list(result_tweet['clean_tweet'])
         senti_analyzer = TextClassifier.load('en-sentiment')
         twitter_flair_score = []
